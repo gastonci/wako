6a25d42ed44450da951e669041d274233d6e2e67
gastonci <aphardsoft@yahoo.com.ar>
2018-06-14 17:30:12 -0300

    new email

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
-var nodemailer = require('nodemailer'); 
+const sendmail = require('sendmail')();
+ 
+sendmail({
+    from: 'no-reply@yourdomain.com',
+    to: 'test@qq.com, test@sohu.com, test@163.com ',
+    subject: 'test sendmail',
+    html: 'Mail of test sendmail ',
+  }, function(err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
+});
 
 app.get('/', function (req, res) {
 	switch(req.query.v) {
 		case "1":
 			res.send("hola")
+			sendmail({
+    			from: 'no-reply@yourdomain.com',
+    			to: 'lalor@loketa.com',
+    			subject: 'test sendmail',
+    			html: 'Mail of test sendmail ',
+  			}, function(err, reply) {
+    			console.log(err && err.stack);
+    			console.dir(reply);
+			})
 			break
 		case "2":
 			res.send("chau")
